bfea0155af3502c7eef772edf5638bde9f01d942
nikolamiros <nikola.miros85@gmail.com>
2019-12-22 23:12:23 +0100

    End of Section 8

--- DatingApp.API/Properties/Data/DataContext.cs
+++ DatingApp.API/Properties/Data/DataContext.cs
@@ -11,5 +11,6 @@ namespace DatingApp.API.Properties.Data
 
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
     }
 }
