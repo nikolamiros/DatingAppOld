1ac10fe6a5be9f6fccb2dee807b34c4cc8c428c5
nikolamiros <nikola.miros85@gmail.com>
2020-03-11 06:49:26 +0100

    End of section 15

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -16,7 +16,7 @@ export class UserService {
 
     constructor(private http: HttpClient) {}
 
-    getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>> {
+    getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
       const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<User[]>();
 
       let params = new HttpParams();
@@ -31,6 +31,14 @@ export class UserService {
         params = params.append('maxAge', userParams.maxAge);
         params = params.append('gender', userParams.gender);
         params = params.append('orderBy', userParams.orderBy);
+      }
+
+      if (likesParam === 'Likers') {
+        params = params.append('likers', 'true');
+      }
+
+      if (likesParam === 'Likees') {
+        params = params.append('likees', 'true');
       }
 
       return this.http.get<User[]>(this.baseUrl + 'users', { observe: 'response', params})
@@ -54,10 +62,14 @@ export class UserService {
     }
 
     setMainPhoto(userId: number, id: number) {
-      return this.http.post(this.baseUrl + 'users/'+ userId + '/photos/' + id + '/setMain', {});
+      return this.http.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {});
     }
 
     deletePhoto(userId: number, id: number) {
       return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
+    }
+
+    sendLike(id: number, recipientId: number)  {
+      return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
     }
 }
